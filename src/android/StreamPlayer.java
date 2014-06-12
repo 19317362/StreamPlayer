12a4a051540bec633c98af2685743c62fec0a024
Justin Liu <yuting1987@gmail.com>
2014-06-12 15:27:10 +0800

    debug

--- src/android/StreamPlayer.java
+++ src/android/StreamPlayer.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.app.AlertDialog;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
